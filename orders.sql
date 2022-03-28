2d51ed46abbef506156574378a0e390b99cdc5af
Matthew Guizar <matthewrguizar@gmail.com>
2022-03-28 11:54:33 -0700

    finished orders section

---
+++ orders.sql
@@ -0,0 +1,26 @@
+CREATE TABLE orders (
+ order_id SERIAL PRIMARY KEY, 
+person_id VARCHAR(20), 
+product_name VARCHAR(30), 
+product_price FLOAT,
+quantity INTEGER
+);
+
+INSERT INTO orders(person_id,
+product_name,
+product_price,
+quantity)
+Values ('bobby', 'soap', 2.99, 2),
+('robby', 'gel', 1.99, 5),
+('joe', 'cement', 20.99, 1),
+('john', 'cat food', 10.99, 3),
+('bobby', 'milk', 3.99, 4)
+
+select * from orders
+
+select SUM(quantity) from orders
+
+SELECT SUM(product_price) FROM orders
+
+select SUM(quantity) from orders
+group by person_id
